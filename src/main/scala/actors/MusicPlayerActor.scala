6fe1e35a939a6c83ab418d739686612c0cba9fa0
Eman <sayedeman98@gmail.com>
2024-12-28 22:38:21 +0800

    play and pause song functions with initial UI

--- src/main/scala/actors/MusicPlayerActor.scala
+++ src/main/scala/actors/MusicPlayerActor.scala
@@ -6,14 +6,67 @@ import javafx.scene.media.{Media, MediaPlayer}
 import protocols.SongProtocols._
 
 object MusicPlayerActor {
-  // Playback states
-//  sealed trait PlaybackState
-//  case object Playing extends PlaybackState
-//  case object Paused extends PlaybackState
-//
-//  private var playbackState: PlaybackState = Paused
+
   private var currentSongId: Option[String] = None
   private var mediaPlayer: Option[MediaPlayer] = None
+
+  def apply(): Behavior[Command] = Behaviors.setup { context =>
+    Behaviors.receiveMessage {
+      case PlaySong(songId, filePath, replyTo) =>
+        // Stop any currently playing song
+        if (mediaPlayer.isDefined) {
+          context.log.info(s"Stopping currently playing song: ${currentSongId.getOrElse("Unknown")}")
+          mediaPlayer.foreach(_.stop())
+          mediaPlayer = None
+          currentSongId = None
+        }
+
+        try {
+          val media = new Media(new java.io.File(filePath).toURI.toString)
+          mediaPlayer = Some(new MediaPlayer(media))
+          mediaPlayer.foreach(_.play())
+          currentSongId = Some(songId)
+          context.log.info(s"Playing song: $songId from $filePath")
+          if (replyTo != null) replyTo ! s"Playing song: $songId"
+        } catch {
+          case e: Exception =>
+            context.log.error(s"Failed to play song: $songId. Error: ${e.getMessage}")
+            if (replyTo != null) replyTo ! s"Failed to play song: $songId"
+        }
+        Behaviors.same
+
+
+      case PauseSong(songId, replyTo) =>
+        if (currentSongId.contains(songId) && mediaPlayer.isDefined) {
+          mediaPlayer.foreach(_.pause())
+          context.log.info(s"Paused song: $songId")
+          if (replyTo != null) replyTo ! s"Paused song: $songId"
+        } else {
+          context.log.error(s"Cannot pause song: $songId. It is not currently playing.")
+          if (replyTo != null) replyTo ! s"Cannot pause song: $songId"
+        }
+        Behaviors.same
+
+
+    }
+  }
+}
+
+
+//        try {
+//          val media = new Media(new java.io.File(filePath).toURI.toString)
+//          mediaPlayer = Some(new MediaPlayer(media))
+//          mediaPlayer.foreach(_.play())
+//          currentSongId = Some(songId)
+//          context.log.info(s"Playing song: $songId from file: $filePath")
+//          replyTo ! s"Playing song: $songId"
+//        } catch {
+//          case e: Exception =>
+//            context.log.error(s"Error playing song $songId: ${e.getMessage}")
+//            //replyTo ! s"Failed to play song: $songId"
+//            if (replyTo != null) replyTo ! s"Failed to play song: $songId"
+//        }
+//        Behaviors.same
 
 //  def apply(): Behavior[Command] = Behaviors.setup { context =>
 //    Behaviors.receiveMessage {
@@ -45,62 +98,3 @@ object MusicPlayerActor {
 //        Behaviors.same
 //    }
 //  }
-
-  def apply(): Behavior[Command] = Behaviors.setup { context =>
-    Behaviors.receiveMessage {
-      case PlaySong(songId, filePath, replyTo) =>
-        // Stop any currently playing song
-        if (mediaPlayer.isDefined) {
-          context.log.info(s"Stopping currently playing song: ${currentSongId.getOrElse("Unknown")}")
-          mediaPlayer.foreach(_.stop())
-          mediaPlayer = None
-          currentSongId = None
-        }
-
-        try {
-          val media = new Media(new java.io.File(filePath).toURI.toString)
-          mediaPlayer = Some(new MediaPlayer(media))
-          mediaPlayer.foreach(_.play())
-          currentSongId = Some(songId)
-          context.log.info(s"Playing song: $songId from $filePath")
-          if (replyTo != null) replyTo ! s"Playing song: $songId"
-        } catch {
-          case e: Exception =>
-            context.log.error(s"Failed to play song: $songId. Error: ${e.getMessage}")
-            if (replyTo != null) replyTo ! s"Failed to play song: $songId"
-        }
-        Behaviors.same
-//        try {
-//          val media = new Media(new java.io.File(filePath).toURI.toString)
-//          mediaPlayer = Some(new MediaPlayer(media))
-//          mediaPlayer.foreach(_.play())
-//          currentSongId = Some(songId)
-//          context.log.info(s"Playing song: $songId from file: $filePath")
-//          replyTo ! s"Playing song: $songId"
-//        } catch {
-//          case e: Exception =>
-//            context.log.error(s"Error playing song $songId: ${e.getMessage}")
-//            //replyTo ! s"Failed to play song: $songId"
-//            if (replyTo != null) replyTo ! s"Failed to play song: $songId"
-//        }
-//        Behaviors.same
-
-      case PauseSong(songId, replyTo) =>
-        if (currentSongId.contains(songId) && mediaPlayer.isDefined) {
-          mediaPlayer.foreach(_.pause())
-          context.log.info(s"Paused song: $songId")
-          if (replyTo != null) replyTo ! s"Paused song: $songId"
-        } else {
-          context.log.error(s"Cannot pause song: $songId. It is not currently playing.")
-          if (replyTo != null) replyTo ! s"Cannot pause song: $songId"
-        }
-        Behaviors.same
-
-//      case PauseSong(songId, replyTo) =>
-//        mediaPlayer.foreach(_.pause())
-//        context.log.info(s"Paused song: $songId")
-//        replyTo ! s"Paused song: $songId"
-//        Behaviors.same
-    }
-  }
-}
